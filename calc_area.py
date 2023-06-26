c02e8e76ce49e470c85dc1ed18124e268fa50924
eMobilis <markerwan2004@gmail.com>
2023-06-26 09:25:36 +0300

    area of a squre

---
+++ calc_area.py
@@ -0,0 +1,12 @@
+#enable user to enter input
+width =input("enter width")
+length = input("enter length")
+#convert two variables into integers
+width = int(width)
+length = int(length)
+
+#perform calculation
+result = length * width
+print("area is", result)
+
+
