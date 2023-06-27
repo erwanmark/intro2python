8efe0613d93043baad50d596bc79356ecb6dcf80
eMobilis <markerwan2004@gmail.com>
2023-06-27 10:15:26 +0300

    changed header name in website

--- betting.py
+++ betting.py
@@ -4,7 +4,7 @@ if system == 1:
     print(" u hae won 500000")
 elif system == 2:
     print(" u have won 1000000")
-elif  system == 3:
+elif system == 3:
     print("u have won 10000000")
 elif system == 4:
     print(" u have won car subaru")
