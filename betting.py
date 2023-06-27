2aa8f1e555745e48d889767dd421f17ca9768a9e
eMobilis <markerwan2004@gmail.com>
2023-06-27 09:44:40 +0300

    my betting system reward

---
+++ betting.py
@@ -0,0 +1,14 @@
+system = int(input("Enter number"))
+
+if system == 1:
+    print(" u hae won 500000")
+elif system == 2:
+    print(" u have won 1000000")
+elif  system == 3:
+    print("u have won 10000000")
+elif system == 4:
+    print(" u have won car subaru")
+elif system == 5:
+    print(" u have won a house")
+else:
+    print("good luck nxt tyme")
