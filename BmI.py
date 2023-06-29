44e7f04125c0dd6804be02ea0d8ad66c565e8d47
eMobilis <markerwan2004@gmail.com>
2023-06-29 09:20:41 +0300

    bmi system

--- BmI.py
+++ BmI.py
@@ -3,10 +3,10 @@ weight = float(input("Enter your weight in kg: "))
 BMI = weight / (height/100)**2
 print(f"You BMI is {BMI}")
 
-if BMI < 18:
+if BMI <= 18:
     print("Underweight")
 elif BMI >= 18.1 <29:
-    print("Normalweight")
+    print("Normal weight")
 elif BMI >= 29.1 <34:
     print("overweight")
 elif BMI >=34:
