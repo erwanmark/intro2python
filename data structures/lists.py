5e89fcd1279f0032b66d025902f7a57251faf3cb
eMobilis <markerwan2004@gmail.com>
2023-06-29 10:30:44 +0300

    updated list

--- data structures/lists.py
+++ data structures/lists.py
@@ -1,8 +1,17 @@
 #defined using squared brackets
 #you can modify data in a lst -add , remove or update
 #a list has an order
-friends = ["Bob","Marie","jake"]
+friends = ["Bob","Marie","jake","Ann","Abigail"]
 friends.append("titus")
 friends.append("mathew")
 friends.remove("jake")
-print(friends)
+print(friends[1])
+print(friends[2])
+print(friends[3])
+print(friends[4])
+print(friends[5])
+
+
+
+
+
