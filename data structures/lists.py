ce919b7d9c05ac4b84edfeb06667768b789247f0
eMobilis <markerwan2004@gmail.com>
2023-06-29 09:53:19 +0300

    lists

---
+++ data structures/lists.py
@@ -0,0 +1,8 @@
+#defined using squared brackets
+#you can modify data in a lst -add , remove or update
+#a list has an order
+friends = ["Bob","Marie","jake"]
+friends.append("titus")
+friends.append("mathew")
+friends.remove("jake")
+print(friends)
