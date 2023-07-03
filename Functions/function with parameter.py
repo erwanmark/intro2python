00f37c11c2f1ee030f168c7cab017e348e738e7b
eMobilis <markerwan2004@gmail.com>
2023-07-03 10:24:56 +0300

    function with parameter

---
+++ Functions/function with parameter.py
@@ -0,0 +1,10 @@
+def addnums(x, y):
+    result = x + y
+    print(result)
+
+addnums(10, 30)
+
+def showname(first, last):
+    print(first, last)
+
+showname(input("enter first name"), input("enter last name"))
