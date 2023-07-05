e8f97757f56a0368267c223e1be521b21824077f
eMobilis <markerwan2004@gmail.com>
2023-07-05 09:16:31 +0300

    module 1

---
+++ loops/for loop 3.py
@@ -0,0 +1,9 @@
+fizz = 1
+while fizz < 102:
+    print(fizz)
+    fizz += 3
+
+buzz = 1
+while buzz < 105:
+    print(buzz)
+    buzz += 5
---
+++ modules/module2.py
@@ -0,0 +1,6 @@
+from module1 import *
+
+
+print(addtwonums(23,456))
+print(subtwonums(1098,678))
+print(multttwonums(234,567))
